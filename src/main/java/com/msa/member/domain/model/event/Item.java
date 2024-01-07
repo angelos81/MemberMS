88b1510dfb42e82881fe7481192080820902b2d8
snoopy <angelos81@gmail.com>
2024-01-07 11:02:22 +0900

    회원 Consumer 작성

---
+++ src/main/java/com/msa/member/domain/model/event/Item.java
@@ -0,0 +1,25 @@
+package com.msa.member.domain.model.event;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Embeddable;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Embeddable
+public class Item {
+    private Integer no;
+    private String title;
+
+
+    // sampleCode
+    public static Item sample() {
+        return new Item(100, "MSA실습");
+    }
+    public static void main(String[] args) {
+        System.out.println(sample().toString());
+    }
+}
