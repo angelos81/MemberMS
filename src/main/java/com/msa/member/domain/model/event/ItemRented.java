26748b5ce20deee7795beaf7844a3191e5efdba1
snoopy <angelos81@gmail.com>
2024-01-07 11:17:55 +0900

    오류 수정

--- src/main/java/com/msa/member/domain/model/event/ItemRented.java
+++ src/main/java/com/msa/member/domain/model/event/ItemRented.java
@@ -3,9 +3,11 @@ package com.msa.member.domain.model.event;
 import com.msa.member.domain.model.vo.IDName;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 import java.io.Serializable;
 
+@NoArgsConstructor
 @AllArgsConstructor
 @Getter
 public class ItemRented implements Serializable {
